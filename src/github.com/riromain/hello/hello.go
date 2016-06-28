1e4c718cbc87f5adefd6afdea45953e46c5ba8c3
RiRomain <romain.rinie@xcom.de>
2016-06-28 19:24:08 +0200

    AddEntry and ReadEntry function
    Correction of AddEntry method.
    Creation of ReadEntry method to read n entry from a given channel.

--- src/github.com/riromain/hello/hello.go
+++ src/github.com/riromain/hello/hello.go
@@ -2,26 +2,19 @@ package main
 
 import ("fmt"
 	"time"
-	"container/list"
 	"database/sql"
 )
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"math/big"
 )
 
-type tempRecord struct {
-	tempEntry list.List
-}
-
 type TempEntry struct {
-	id big.Int
 	channel string
 	date time.Time
 	temp float32
 }
 
-func addEntry(temperature float32, channel string, db sql.DB) {
+func addEntry(temperature float32, channel string, db *sql.DB) {
 	stmtIns, err := db.Prepare("INSERT INTO TEMP_ENTRY VALUES(0, ?, ?, ?)")
 	if err != nil {
 		panic(err.Error())
@@ -35,7 +28,34 @@ func addEntry(temperature float32, channel string, db sql.DB) {
 	}
 }
 
-func readEntries(maxEntry int, )
+func readEntries(maxEntry int, channel string, db *sql.DB) ([]TempEntry){
+	stmtOut, err := db.Prepare("SELECT CREATION_TIME, TEMPERATURE FROM TEMP_ENTRY WHERE CHANNEL = ? ORDER BY ID DESC LIMIT ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtOut.Close()
+
+	rows, err := stmtOut.Query(channel, maxEntry)
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer rows.Close()
+	temperature := []TempEntry{}
+	for rows.Next() {
+		var actualEntry TempEntry
+		err = rows.Scan(&actualEntry.date, &actualEntry.temp)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		actualEntry.channel = channel
+		temperature = append(temperature, actualEntry)
+	}
+	err = rows.Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return temperature
+}
 
 func main() {
 
@@ -44,48 +64,10 @@ func main() {
 		panic(err.Error())
 	}
 	defer db.Close()
-	addEntry(23.2, "BIG_THANK", db)
 
-	stmtOut, err := db.Prepare("SELECT TEMPERATURE FROM TEMP_ENTRY ORDER BY ID DESC LIMIT 1")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	defer stmtOut.Close()
+	addEntry(23.2, "BIG_TANK", db)
+	tempEntry := readEntries(5, "BIG_TANK", db)
+	fmt.Println(tempEntry)
 
-	var outTemp float32
-	err = stmtOut.QueryRow().Scan(&outTemp) // WHERE number = 13
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	fmt.Println("The square number of 13 is: ", outTemp)
-
-    	stmtOut, err = db.Prepare("SELECT CREATION_TIME, TEMPERATURE FROM TEMP_ENTRY WHERE CHANNEL = ? ORDER BY ID DESC LIMIT ?")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	defer stmtOut.Close()
-
-	rows, err := stmtOut.Query("BIG_TANK", 10)
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	defer rows.Close()
-
-	var (
-		creationTime time.Time
-		temperature float32
-	)
-
-	for rows.Next() {
-		err := rows.Scan(&creationTime, &temperature)
-		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
-		}
-		fmt.Println(creationTime, temperature)
-	}
-	err = rows.Err()
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
 }
 

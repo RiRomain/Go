379d0e775fab62bcf6b8805dc97e387d281b9b01
RiRomain <romain.rinie@xcom.de>
2016-06-28 07:37:02 +0200

    Multi result statement
    http://golang-basic.blogspot.de/2014/06/golang-database-step-by-step-guide-on.html
    To create object:
    
    http://stackoverflow.com/questions/26617957/how-to-scan-a-mysql-timestamp-value-into-a-time-time-variable

--- src/github.com/riromain/hello/hello.go
+++ src/github.com/riromain/hello/hello.go
@@ -5,44 +5,52 @@ import ("fmt"
 	"container/list"
 	"database/sql"
 )
-import _ "github.com/go-sql-driver/mysql"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"math/big"
+)
 
 type tempRecord struct {
 	tempEntry list.List
 }
 
 type TempEntry struct {
-	temp float32
-	date time.Time
+	id big.Int
 	channel string
+	date time.Time
+	temp float32
 }
 
-func main() {
-
-	db, err := sql.Open("mysql", "temperature:local/temperature")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-
+func addEntry(temperature float32, channel string, db sql.DB) {
 	stmtIns, err := db.Prepare("INSERT INTO TEMP_ENTRY VALUES(0, ?, ?, ?)")
 	if err != nil {
 		panic(err.Error())
 	}
-
 	defer stmtIns.Close()
+	var datetime = time.Now()
+	datetime.Format(time.RFC3339)
+	_, err = stmtIns.Exec(channel, datetime, temperature)
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+}
+
+func readEntries(maxEntry int, )
+
+func main() {
+
+	db, err := sql.Open("mysql", "user:password@tcp(xxx.xxx.xxx.xxx:XXXX)/dbName?parseTime=true")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	addEntry(23.2, "BIG_THANK", db)
 
 	stmtOut, err := db.Prepare("SELECT TEMPERATURE FROM TEMP_ENTRY ORDER BY ID DESC LIMIT 1")
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
 	defer stmtOut.Close()
-	var datetime = time.Now()
-	datetime.Format(time.RFC3339)
-	_, err = stmtIns.Exec("BIG_THANK", datetime, 23.2) // Insert tuples (i, i^2)
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
 
 	var outTemp float32
 	err = stmtOut.QueryRow().Scan(&outTemp) // WHERE number = 13
@@ -51,6 +59,33 @@ func main() {
 	}
 	fmt.Println("The square number of 13 is: ", outTemp)
 
-    	fmt.Println("hello, world\n")
+    	stmtOut, err = db.Prepare("SELECT CREATION_TIME, TEMPERATURE FROM TEMP_ENTRY WHERE CHANNEL = ? ORDER BY ID DESC LIMIT ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtOut.Close()
+
+	rows, err := stmtOut.Query("BIG_TANK", 10)
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer rows.Close()
+
+	var (
+		creationTime time.Time
+		temperature float32
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&creationTime, &temperature)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		fmt.Println(creationTime, temperature)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
 }
 
